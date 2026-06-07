7579366081dc3fe7e849d431b522b4eb8343a128
Вячеслав Лебедев <vaceslavlebedev825@gmail.com>
2026-06-07 15:45:11 +0500

    feat: implement file reading and json parsing in genDiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import genDiff from "../src/index.js"; // Импортируем нашу библиотеку
 
 const program = new Command();
 
@@ -11,9 +12,9 @@ program
   .arguments("<filepath1> <filepath2>")
   .option("-f, --format [type]", "output format")
   .helpOption("-h, --help", "display help for command")
-  .action((filepath1, filepath2, options) => {
-    // Здесь я позже буду вызывать основную функцию genDiff
-    // console.log(filepath1, filepath2, options.format);
+  .action((filepath1, filepath2) => {
+    // Передаем пути к файлам в главную функцию
+    genDiff(filepath1, filepath2);
   });
 
 program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import path from "path";
+
 const genDiff = (filePath1, filePath2) => {
-  // Логика сравнения будет здесь в следующих шагах
+  const absolutePath1 = path.resolve(process.cwd(), filePath1);
+  const absolutePath2 = path.resolve(process.cwd(), filePath2);
+
+  const data1 = fs.readFileSync(absolutePath1, "utf-8");
+  const data2 = fs.readFileSync(absolutePath2, "utf-8");
+
+  const obj1 = JSON.parse(data1);
+  const obj2 = JSON.parse(data2);
+
+  console.log("Parsed file 1:", obj1);
+  console.log("Parsed file 2:", obj2);
+
   return "";
 };
 
